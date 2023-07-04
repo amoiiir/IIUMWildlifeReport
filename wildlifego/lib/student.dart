ebafcc1368bc9625a7184668e6a89cfe62d775fa
Amir Hamzah <101052053+amoiiir@users.noreply.github.com>
2023-07-04 16:41:13 +0800

    Homepage with routing to camera

--- wildlifego/lib/student.dart
+++ wildlifego/lib/student.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:wildlifego/login.dart';
+import 'package:camera/camera.dart';
 
+import 'Screen/camera.dart';
 import 'login.dart';
 
 class Student extends StatefulWidget {
@@ -10,7 +13,44 @@ class Student extends StatefulWidget {
   State<Student> createState() => _StudentState();
 }
 
+Future<void> logout(BuildContext context) async {
+  const CircularProgressIndicator();
+  await FirebaseAuth.instance.signOut();
+  Navigator.pushReplacement(
+    context,
+    MaterialPageRoute(
+      builder: (context) => const LoginPage(),
+    ),
+  );
+}
+
 class _StudentState extends State<Student> {
+  late List<CameraDescription> cameras;
+  late CameraController _cameraController;
+  bool _isCameraInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeCamera();
+  }
+
+  Future<void> initializeCamera() async {
+    cameras = await availableCameras();
+    _cameraController = CameraController(cameras[0], ResolutionPreset.medium);
+    await _cameraController.initialize();
+    if (!mounted) return;
+    setState(() {
+      _isCameraInitialized = true;
+    });
+  }
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,16 +67,76 @@ class _StudentState extends State<Student> {
           )
         ],
       ),
-    );
-  }
-
-  Future<void> logout(BuildContext context) async {
-    const CircularProgressIndicator();
-    await FirebaseAuth.instance.signOut();
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(
-        builder: (context) => const LoginPage(),
+      //floating action button must be center
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    CameraPage(cameraController: _cameraController),
+              ));
+        },
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 10.0,
+        child: Container(
+          height: 60.0,
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                    // crossAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      MaterialButton(
+                        minWidth: 150,
+                        onPressed: () {
+                          setState(() {
+                            // currentScreen = const Home();
+                            // currentTab = 0;
+                          });
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.home,
+                              color: Colors.blueGrey,
+                            ),
+                            Text('Home')
+                          ],
+                        ),
+                      )
+                    ]),
+                //right tab bar icons
+                Row(
+                    // crossAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      MaterialButton(
+                        minWidth: 150,
+                        onPressed: () {
+                          setState(() {
+                            // currentScreen = const Home();
+                            // currentTab = 0;
+                          });
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.assignment,
+                              color: Colors.blueGrey,
+                            ),
+                            Text('My Reports')
+                          ],
+                        ),
+                      )
+                    ])
+              ]),
+        ),
       ),
     );
   }
