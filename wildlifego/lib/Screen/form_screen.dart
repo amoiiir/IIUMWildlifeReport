1b7d7a462d7022ce4cf6764fd1ed1194c03c7cf1
Firdaus Shahrum <firdausshahrum21@gmail.com>
2023-07-06 15:44:49 +0800

    Form can be filled now

--- wildlifego/lib/Screen/form_screen.dart
+++ wildlifego/lib/Screen/form_screen.dart
@@ -10,68 +10,83 @@ class FormScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Issue Report')),
-      body: Container(
-        child: GestureDetector(
-          child: Column(
-            children: [
-              Expanded(
-                child: Center(
-                  heightFactor: 1.5,
+      body: ListView(
+        children: [
+          Container(
+            child: Column(
+              children: [
+                Center(
+                  //heightFactor: 1.5,
                   child: Container(
-                    width: 144,
-                    height: 256,
-                    child: Image.file(imageFile),
+                    width: 256,
+                    height : 256,
+                    child: Image.file(
+                      imageFile,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Title',
-                    border: OutlineInputBorder(),
+                Form(
+                  key: key,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'Title',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'Animal Type',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'Location',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'Description',
+                            border: OutlineInputBorder(),
+                          ),
+                          maxLines: 3,
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Container(
+                          margin: const EdgeInsets.only(bottom: 20),
+                          alignment: Alignment.bottomCenter,
+                          child: FloatingActionButton.large(
+                            backgroundColor: Colors.white,
+                            onPressed: () async {
+                              // Handle form submission
+                            },
+                            child: const Icon(Icons.done, color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Location',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Description',
-                    border: OutlineInputBorder(),
-                  ),
-                  maxLines: 3,
-                ),
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  margin: const EdgeInsets.only(bottom: 20),
-                  alignment: Alignment.bottomCenter,
-                  child: FloatingActionButton.large(
-                    backgroundColor: Colors.white,
-                    onPressed: () async {
-                      // Handle form submission
-                    },
-                    child: const Icon(Icons.done, color: Colors.black),
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
-          onDoubleTap: () {
-            Navigator.pop(context);
-          },
-        ),
+        ],
       ),
     );
   }
